e8a450a7b36c2447c04e3c504d443b7533b51759
Shuvro Dev Mondal <shuvrodevmondal337@gmail.com>
2025-05-03 03:01:34 +0600

    added token with header

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,10 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
 
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:5000/api/v1",
+  credentials: "include", // if cookies sent by backend, this option sets cookie in the browser cookie
+  prepareHeaders: (headers, { getState }) => {
+    // method to attach token with every request
+    const token = (getState() as RootState).auth.token; // get the token from auth state
+
+    if (token) {
+      headers.set("authorization", token); // set token with headers
+    }
+    return headers;
+  },
+});
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
-    credentials: "include", // if cookies sent by backend, this option sets cookie in the browser cookie
-  }),
+  baseQuery: baseQuery,
   endpoints: () => ({}), // endpoints Are being injected
 });
