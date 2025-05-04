546f1ab1cb80cf2da450eb5944f098c470f63ea0
Shuvro Dev Mondal <shuvrodevmondal337@gmail.com>
2025-05-05 02:44:17 +0600

    finished auth setup

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,13 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  createApi,
+  DefinitionType,
+  FetchArgs,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { logOut, setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -14,8 +22,44 @@ const baseQuery = fetchBaseQuery({
     return headers;
   },
 });
+
+/* custom made baseQuery(baseQueryWithrefreshToken) logic => 
+ Creating a custom function(baseQueryWithrefreshToken) layer to detect access+refresh token expirity, 
+ This functionchecks if any baseQuery to any auth protected route(backend)
+ gets Unauthorized error/access token expirity error.
+
+ If Yes then we can 
+ 1.send the refresh token from the cookies to the /refresh-token route 
+ 2. if refresh token is ok, set the user with user from state and accessToken as the renewed accessToken, conduct basequery again.
+ 3. if refresh token is also expired , the respose from '/refresh-token' will not contain accessToken, so we logout the user.
+*/
+const baseQueryWithrefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  let result = await baseQuery(args, api, extraOptions); // have to pass thes 3 arguments as redux required
+  // 401 =>  Unauthorized(accesstoken is expired)
+  if (result?.error?.status === 401) {
+    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include", // to send cookies
+    });
+    const data = await res.json();
+
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).auth.user;
+      api.dispatch(setUser({ user, token: data.data.accessToken }));
+      result = await baseQuery(args, api, extraOptions); //  send request again, baseQuery will attach request with renewed accessToken
+    } else {
+      api.dispatch(logOut());
+    }
+  }
+  return result;
+};
+
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithrefreshToken,
   endpoints: () => ({}), // endpoints Are being injected
 });
