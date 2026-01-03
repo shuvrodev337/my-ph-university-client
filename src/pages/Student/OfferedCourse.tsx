15804a503f09815efa03c76cefa1f51b96c11ee4
Shuvro Dev Mondal <shuvrodevmondal337@gmail.com>
2026-01-04 03:27:34 +0600

    offered course table

--- src/pages/Student/OfferedCourse.tsx
+++ src/pages/Student/OfferedCourse.tsx
@@ -1,29 +1,67 @@
+import { Button, Col, Row } from "antd";
 import { useGetMyOfferedCoursesQuery } from "../../redux/features/student/courseRelatedApi";
-
+type TAcc = {
+  [index: string]: any;
+};
 const OfferedCourse = () => {
   const { data: offeredCourses } = useGetMyOfferedCoursesQuery(undefined);
 
-  const singleObject = offeredCourses?.data?.reduce((acc, item) => {
+  const singleObject = offeredCourses?.data?.reduce((acc: TAcc, item) => {
     console.log(item);
     const key = item?.course?.title;
     acc[key] = acc[key] || { courseTitle: key, sections: [] }; // used || to to avoid replacing the object and making the sections: [] everytime loop iterates
     acc[key].sections.push({
       section: item.section,
       _id: item._id,
+      days: item.days,
+      startTime: item.startTime,
+      endTime: item.endTime,
     });
     return acc;
   }, {});
-  console.log(singleObject);
+  // console.log(singleObject);
   // console.log(Object.values(singleObject ? singleObject : {}));
+  const modifiedData = Object.values(singleObject ? singleObject : {});
   return (
-    <div>
-      <h1>This is student offered course</h1>
-    </div>
+    <Row gutter={[0, 20]}>
+      {modifiedData?.map((item) => {
+        return (
+          <Col span={24} style={{ border: "solid #d4d4d4 1px" }}>
+            <div style={{ padding: "10px" }}>
+              <h2> {item.courseTitle} </h2>
+            </div>
+            <div>
+              {item?.sections.map((section) => {
+                return (
+                  <Row
+                    justify={"space-between"}
+                    align={"middle"}
+                    style={{ border: "solid #d4d4d4 1px", padding: "10px" }}
+                  >
+                    <Col span={5}>Section : {section.section}</Col>
+                    <Col span={5}>
+                      Days:{" "}
+                      {section.days.map((day) => (
+                        <span> {day} </span>
+                      ))}
+                    </Col>
+                    <Col span={5}>Start time : {section.startTime}</Col>
+                    <Col span={5}>End time : {section.endTime}</Col>
+                    <Button>Enroll</Button>
+                  </Row>
+                );
+              })}
+            </div>
+          </Col>
+        );
+      })}
+    </Row>
   );
 };
 
 export default OfferedCourse;
 
+/*
 // *****getting data in this format****** =>
 const exampleData1 = [
   { _id: "sdcsdsdsd", course: { title: "Civil" }, section: 1 },
@@ -62,3 +100,4 @@ const exampleData2 = [
 ];
 
 // console.log(exampleData1, exampleData2);
+*/
